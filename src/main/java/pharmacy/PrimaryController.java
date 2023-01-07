a6e2d3f2d072c81667255a084f487d9421b347fe
malakbdj <117923180+malakbdj@users.noreply.github.com>
2023-01-07 23:55:02 +0100

    Add files via upload

---
+++ src/main/java/pharmacy/PrimaryController.java
@@ -0,0 +1,192 @@
+
+    package pharmacy;
+
+    import javafx.fxml.FXML;
+    import javafx.scene.control.Label;
+    import javafx.scene.control.TextField;
+    import javafx.scene.layout.Pane;
+    
+    public class PrimaryController {
+    
+        @FXML
+        private Label Configuration;
+    
+        @FXML
+        private Pane Configurationbut;
+    
+        @FXML
+        private Label CustomerManagement;
+    
+        @FXML
+        private Pane CustomerManagementbut;
+    
+        @FXML
+        private Label Inventory;
+    
+        @FXML
+        private Label Reports;
+    
+        @FXML
+        private Pane Reportsbut;
+    
+        @FXML
+        private Label TotalnoofSuppliers;
+    
+        @FXML
+        private Label TotalnoofUsers;
+    
+        @FXML
+        private Label a;
+    
+        @FXML
+        private Label a0;
+    
+        @FXML
+        private Label a8;
+    
+        @FXML
+        private Label a9;
+    
+        @FXML
+        private Pane b0;
+    
+        @FXML
+        private Pane b1;
+    
+        @FXML
+        private Pane b2;
+    
+        @FXML
+        private Pane b3;
+    
+        @FXML
+        private Pane b4;
+    
+        @FXML
+        private Pane b5;
+    
+        @FXML
+        private Pane b6;
+    
+        @FXML
+        private Pane b7;
+    
+        @FXML
+        private Pane b8;
+    
+        @FXML
+        private Pane b9;
+    
+        @FXML
+        private Pane bb;
+    
+        @FXML
+        private Pane bd;
+    
+        @FXML
+        private Pane bok;
+    
+        @FXML
+        private Pane calc;
+    
+        @FXML
+        private TextField calcafi;
+    
+        @FXML
+        private Label d;
+    
+        @FXML
+        private Label dashbord;
+    
+        @FXML
+        private Pane dashbordbu;
+    
+        @FXML
+        private Label dfb;
+    
+        @FXML
+        private Label dfs;
+    
+        @FXML
+        private Label ds;
+    
+        @FXML
+        private Label faive;
+    
+        @FXML
+        private Label fd;
+    
+        @FXML
+        private Label fdd;
+    
+        @FXML
+        private Pane ff;
+    
+        @FXML
+        private Label ffs;
+    
+        @FXML
+        private Label ffx;
+    
+        @FXML
+        private Label gd;
+    
+        @FXML
+        private Pane inventorybut;
+    
+        @FXML
+        private Label mypharmacy;
+    
+        @FXML
+        private Pane navphar;
+    
+        @FXML
+        private Pane navpharmacy;
+    
+        @FXML
+        private Pane navsecond;
+    
+        @FXML
+        private Label num;
+    
+        @FXML
+        private Label numofmedi;
+    
+        @FXML
+        private Label ok;
+    
+        @FXML
+        private Label one;
+    
+        @FXML
+        private Label phore;
+    
+        @FXML
+        private Label se;
+    
+        @FXML
+        private Label seven;
+    
+        @FXML
+        private Label six;
+    
+        @FXML
+        private Label text;
+    
+        @FXML
+        private Label three;
+    
+        @FXML
+        private Label tow;
+    
+        @FXML
+        private Label txtegrou;
+    
+        @FXML
+        private Label w;
+    
+        @FXML
+        private Label x;
+    
+    }
+    
---
+++ src/main/java/pharmacy/SecondaryController.java
@@ -0,0 +1,23 @@
+package pharmacy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class SecondaryController {
+
+    @FXML
+    private Pane bor;
+
+    @FXML
+    private Button logni;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private PasswordField pass;
+
+}
